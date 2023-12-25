39f32fc6273189c68cdb347b3133c19b981ebe38
Chang <47656782+changyyun@users.noreply.github.com>
2023-12-24 20:55:45 -0800

    added www subdomain

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,5 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
     //site: 'https://changyyun.github.io',
-    site: 'https://changy.dev',
+    site: 'https://www.changy.dev',
 });
