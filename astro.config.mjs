7c69bb00b474c2d6bac162587e2b9d323ff16eda
Chang <47656782+changyyun@users.noreply.github.com>
2023-12-25 16:22:05 -0800

    revert to github.io domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,5 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://changy.dev'
+    site: 'https://changyyun.github.io'
 });
