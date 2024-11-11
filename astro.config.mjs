108637733c3595258798e712bdfe4b2386ea4aca
Chang Yun <47656782+changyyun@users.noreply.github.com>
2024-11-10 19:52:45 -0800

    Change config to use custom domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,5 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://changyyun.github.io',
+    site: 'https://changy.dev',
 });
